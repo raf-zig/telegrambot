a4b073e1e0118fba13aa1d73fb591f5d92cee5a2
Ravil Ziganshin <84528349+raf-zig@users.noreply.github.com>
2023-10-06 17:52:31 +0500

    refactoring

--- main.rb
+++ main.rb
@@ -15,26 +15,26 @@ Telegram::Bot::Client.run(token) do |bot|
       )
     else
       arr = message.text.split(' ')
-      r_line_1 = arr[0].to_f
-      r_line_2 = arr[1].to_f
-      r_line_1_2 = arr[2].to_f
-      r_lines = arr[3].to_f
+      line_resistance_1 = arr[0].to_f
+      line_resistance_2 = arr[1].to_f
+      line_resistance_1_2 = arr[2].to_f
+      lines_resistance = arr[3].to_f
       line_length = arr[4].to_f
      
       line_diameter = arr[5].to_f
       temperature = arr[6].to_f
 
-      r_line_1_to_20 = r_line_1 / (1 + 0.006 * (temperature - 20))
-      r_line_2_to_20 = r_line_2 / (1 + 0.006 * (temperature - 20))
-      r_line_1_2_to_20 = r_line_1_2 / (1 + 0.006 * (temperature - 20))
+      line_resistance_1_to_20 = line_resistance_1 / (1 + 0.006 * (temperature - 20))
+      line_resistance_2_to_20 = line_resistance_2 / (1 + 0.006 * (temperature - 20))
+      line_resistance_1_2_to_20 = line_resistance_1_2 / (1 + 0.006 * (temperature - 20))
 
-      l_line_1 = r_line_1_to_20 * line_length
-      l_line_2 = r_line_2_to_20 * line_length
-      l_line_1_2 = r_line_1_2_to_20 * line_length
+      line_1_to_km = line_resistance_1_to_20 * line_length
+      line_2_to_km = line_resistance_2_to_20 * line_length
+      line_1_2_to_km = line_resistance_1_2_to_20 * line_length
 
       coefficient = 1 / (1 + 0.004 * (temperature - 20))
 
-      r_lines_to_20 = r_lines * coefficient / line_length
+      lines_resistance_to_20 = lines_resistance * coefficient / line_length
 
       norm_for_lines = 46 / line_diameter**2
 
@@ -45,19 +45,19 @@ Telegram::Bot::Client.run(token) do |bot|
         text: "Получи результат: 1) Проверка электрического сопротивления изоляции 2.13.1                                                                                        
         
         Сопротивление  изоляции в МОм приведенное к температуре +20С 
-        - жила_1-земля - #{r_line_1_to_20.round(0)}
-        - жила_2-земля - #{r_line_2_to_20.round(0)}
-        - жила_1-жила_2 - #{r_line_1_2_to_20.round(0)}
+        - жила_1-земля - #{line_resistance_1_to_20.round(0)}
+        - жила_2-земля - #{line_resistance_2_to_20.round(0)}
+        - жила_1-жила_2 - #{line_resistance_1_2_to_20.round(0)}
 
         2) Перерасчет электрического сопротивления 1 км изоляции жил кабеля, МОм/км
-        - жила_1-земля - #{l_line_1.round(0)}
-        - жила_2-земля - #{l_line_2.round(0)}
-        - жила_1-жила_2 - #{l_line_1_2.round(0)}
+        - жила_1-земля - #{line_1_to_km.round(0)}
+        - жила_2-земля - #{line_2_to_km.round(0)}
+        - жила_1-жила_2 - #{line_1_2_to_km.round(0)}
         
         3) Измерение электрического сопротивления шлейфа пары 2.13.2
         
         Измерение электрического сопротивления шлейфа пары приведенное к температуре +20С, Ом/км
-        - #{r_lines_to_20.round(0)}
+        - #{lines_resistance_to_20.round(0)}
         
         4) Норма для данного кабеля Инф1-Инф2 (R шл/км <= 46/d2)
         -  не более #{norm_for_lines.round(0)}
